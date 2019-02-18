b2a80ba64130ede847f32ff88eab240537b5f267
Yuris <trueshot86@gmail.com>
2019-02-18 11:06:12 +0000

    WIP

--- README.md
+++ README.md
@@ -20,3 +20,22 @@ $ (export LC_ALL=ja_JP.utf8)
 - python server.py
   - run flask.
 
+## tips
+- install mysql
+  - curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
+  - yum install MariaDB-server MariaDB-client -y
+  - systemctl start mariadb
+  - systemctl enable mariadb
+  - mysql_secure_installation
+
+- setting mysql
+  - create database infra;
+  - create table dns(id integer primary key auto_increment, ip text, domain text, mac text, username text, dhcp_flag int default 0, lock_flag int default 0);
+  - insert into dns(ip, domain, username) values('192.168.100.1','yamaha-rtx.xxx','system_admin');
+  - select * from dns;
+
+- to use sqlalchemy
+  - pip install sqlalchemy
+  - yum install MariaDB-shared MariaDB-devel -y
+  - pip install mysqlclient
+
--- flask_site/views.py
+++ flask_site/views.py
@@ -1,7 +1,25 @@
-from flask import request, redirect, url_for, render_template, flash, session
+from flask import request, redirect, url_for, render_template, flash, session, jsonify
 from flask_site import app
+from sqlalchemy import create_engine
+import json
 
 @app.route('/')
 def show_entries():
     return render_template('entries/index.html')
+
+@app.route('/', methods=['post'])
+def update_list():
+    a = request.json['a']
+    b = request.json['b']
+    c = request.json['c']
+    print(a)
+    print(b)
+    print(c)
+    engine = create_engine('mysql://root:pass@localhost/infra?charset=utf8mb4')
+    with engine.connect() as con:
+        rows = con.execute("select * from dns")
+    for i in rows:
+        print(i)
+    return_data = {"a":'{}'.format(a), "b":'{}'.format(b), "c":'{}'.format(c)}
+    return jsonify(ResultSet=json.dumps(return_data))
 
