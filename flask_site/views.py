d89b4404fdfdeaf716a7360c79e677e647e422b6
Yuris <trueshot86@gmail.com>
2019-02-20 07:20:28 +0000

    torima kansei.

--- flask_site/views.py
+++ flask_site/views.py
@@ -62,8 +62,8 @@ def update_list():
     print('update done !!!!!!!!!!!!!!!')
 
 
-    return jsonify(ResultSet=json.dumps({"a" :id, "b": 'yabai'}))
-#    return jsonify(ResultSet=json.dumps({"id": id, "ip": ip, "domain": domain, "username": username}))
+#    return jsonify(ResultSet=json.dumps({"a" :id, "b": 'yabai'}))
+    return jsonify(ResultSet=json.dumps({"id": id, "ip": ip, "domain": domain, "username": username}))
 
 
 
