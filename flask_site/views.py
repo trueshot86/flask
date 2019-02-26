7a81f452fbef53690bb1a1be321fc4eb4d7bdc57
Yuris <trueshot86@gmail.com>
2019-02-26 10:17:13 +0000

    sippai

--- flask_site/views.py
+++ flask_site/views.py
@@ -3,6 +3,7 @@ from flask_site import app, socketio
 from sqlalchemy import create_engine
 import json
 from flask_socketio import send, emit
+import subprocess
 
 @app.route('/')
 def show_entries():
@@ -37,39 +38,75 @@ def get_list():
 
 @app.route('/apply', methods=['post'])
 def update_list():
-    flash('this is flash test')
-##    make_lock()
-##    lock_flag = check_lock()
-    id = request.json['id']
-    ip = request.json['ip']
-    domain = request.json['domain']
-    username = request.json['username']
+    boke = subprocess.run("date +%S_%3N", shell= True, stdout=subprocess.PIPE)
+    boke = boke.stdout.strip().decode('utf8')
 
-    print('--- --- --- --- --- ')
-    print(id)
-    print(ip)
-    print(domain)
-    print(username)
-    print('--- --- --- --- --- ')
+    make_lock(boke)
+    lock_flag = check_lock()
 
+    if 2 < lock_flag:
+#    if 1 > 0:
+        delete_lock(boke)
+        return 'hage'
+    else:
+        id = request.json['id']
+        ip = request.json['ip']
+        domain = request.json['domain']
+        username = request.json['username']
+    
+        print('--- --- --- --- --- ')
+        print(id)
+        print(ip)
+        print(domain)
+        print(username)
+        print('--- --- --- --- --- ')
+    
+    
+        engine = create_engine('mysql://root:pass@localhost/infra?charset=utf8mb4')
+        with engine.connect() as con:
+            try:
+                rows = con.execute("update dns set ip = '{}', domain = '{}', username = '{}' where id = '{}' ".format(ip, domain, username, id))
+            except:
+                print('--- --- --- --- --- ')
+                print('update done !!!!!!!!!!!!!!!')
+                print('--- --- --- --- --- ')
+                import traceback
+                traceback.print_exc()
+        print('update done !!!!!!!!!!!!!!!')
+        update_dns()
+        delete_lock(boke)
+        modal_form(id, ip, domain, username)
+        not_modal_form(id, domain, username)
+    
+    
+        engine = create_engine('mysql://root:pass@localhost/infra?charset=utf8mb4')
+        with engine.connect() as con:
+            rows = con.execute("select ip, domain, username from dns where id = '{}'".format(ip, domain, username, id))
+        for i in rows:
+            ip = i[0]
+            domain = i[1]
+            username = i[2]
+    
+    #    return jsonify(ResultSet=json.dumps({"a" :id, "b": 'yabai'}))
+        return jsonify(ResultSet=json.dumps({"id": id, "ip": ip, "domain": domain, "username": username}))
 
-    engine = create_engine('mysql://root:pass@localhost/infra?charset=utf8mb4')
-    with engine.connect() as con:
-        try:
-            rows = con.execute("update dns set ip = '{}', domain = '{}', username = '{}' where id = '{}' ".format(ip, domain, username, id))
-        except:
-            print('--- --- --- --- --- ')
-            print('update done !!!!!!!!!!!!!!!')
-            print('--- --- --- --- --- ')
-            import traceback
-            traceback.print_exc()
-    print('update done !!!!!!!!!!!!!!!')
-    modal_form(id, ip, domain, username)
-    not_modal_form(id, domain, username)
+def make_lock(arg):
+    res = subprocess.run('ssh 192.168.100.53 "touch lock/{}"'.format(arg), shell=True)
+    
 
+def check_lock():
+    res = subprocess.run('ssh 192.168.100.53 "ls lock| wc -l"', shell=True, stdout=subprocess.PIPE)
+    print(res)
+    return(int(res.stdout.strip().decode('utf8')))
 
-#    return jsonify(ResultSet=json.dumps({"a" :id, "b": 'yabai'}))
-    return jsonify(ResultSet=json.dumps({"id": id, "ip": ip, "domain": domain, "username": username}))
+def delete_lock(arg):
+    res = subprocess.run('ssh 192.168.100.53 "rm lock/{}"'.format(arg), shell=True, stdout=subprocess.PIPE)
+    
+
+def update_dns():
+    subprocess.run('ssh 192.168.100.53 "touch hage"', shell=True)
+    print('updateeeeeeeeeeeeeeeeeeeeeee')
+    
 
 @app.route('/clear', methods=['post'])
 def clear_form():
